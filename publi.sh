87eb55c2be113dac57b1e52b4fe2e650b8c6758d
Michael <smmbox@gmail.com>
2019-12-02 12:45:23 +0300

    up

--- publi.sh
+++ publi.sh
@@ -1,3 +1,6 @@
+echo 'building...\n'
 go build -o ./bin/srv
 zip -r ./bin/srv.zip ./bin/srv ./resources
-curl --upload-file ./bin/srv https://transfer.sh/srv
+echo 'uploading...\n'
+echo `curl --upload-file ./bin/srv.zip https://transfer.sh/srv.zip`
+echo '\ndone\n'
