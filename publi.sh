1a84d94e8e840ae85b9123478a1b910673541c56
Michael <smmbox@gmail.com>
2019-12-02 12:59:03 +0300

    update

--- publi.sh
+++ publi.sh
@@ -1,12 +1,12 @@
 echo "building..."
 go build -o ./bin/srv
 echo "copying resources..."
-cp ./resources ./bin/resources
+cp -r ./resources ./bin/resources
 cd ./bin
 echo "zipping..."
 rm ./srv.zip
-zip -r ./srv.zip *
+zip -r ./srv.zip ./srv ./resources
+cd ../
 echo "uploading...\n"
 echo `curl --upload-file ./bin/srv.zip https://transfer.sh/srv.zip`
-cd ../
 echo "\ndone\n"
